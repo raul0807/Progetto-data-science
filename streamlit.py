89d635dc1b2ca5d516a42d4b0d36e1f27fc577f4
raul0807 <raul.spano@studenti.unimi.it>
2024-11-14 14:41:00 +0100

    manca l'ultimo file

--- streamlit.py
+++ streamlit.py
@@ -59,7 +59,7 @@ def random():
     st.pyplot(fig)
 
 def customer_preference():
-    st.title("Allocazione e guadagni in base alle preferenze degli ospiti")
+    st.title("Allocazione e guadagni con 'customer preference'")
     
     # Copia il blocco di codice del secondo modulo
     hotel_ex['stanze_disponibili'] = hotel_ex['rooms'].copy()
@@ -108,11 +108,66 @@ def customer_preference():
     ax.set_title('Soddisfazione degli Ospiti')
     st.pyplot(fig)
 
+
+def price():
+    hotel_ex, guest_ex, preferences_ex = carica_file()
+    st.title("Allocazioni e guadagni con 'price'")
+    hotel_ex['stanze_disponibili']=hotel_ex['rooms'].copy()
+    hotel_ex=hotel_ex.sort_values(by='price')   
+    guadagni_hotel= {hotel: 0 for hotel in hotel_ex['hotel']}
+    ospiti_allocati, stanze_occupate, hotel_occupati, ospiti_soddisfatti, allocazioni = stats()
+    for _, guest_row in guest_ex.iterrows():
+        guest=guest_row['guest']
+        discount=guest_row['discount']
+        preferenze_ospite = preferences_ex[preferences_ex['guest'] == guest]
+        hotels_preferiti=preferenze_ospite[preferenze_ospite['hotel'].isin(hotel_ex[hotel_ex['stanze_disponibili']>0]['hotel'])]
+        if not hotels_preferiti.empty:
+            hotels_preferiti=hotels_preferiti.merge(hotel_ex[['hotel', 'price']], on='hotel').sort_values(by='price')
+            hotel_selezionato=hotels_preferiti.iloc[0]['hotel']
+            if hotel_selezionato in preferenze_ospite['hotel'].values:
+                ospiti_soddisfatti += 1
+        else:
+            continue
+        prezzo_hotel=hotel_ex[hotel_ex['hotel']==hotel_selezionato].iloc[0]['price']
+        prezzo_finale=prezzo_hotel*(1-discount)
+        allocazioni.append({
+            'cliente':guest,
+            'hotel_f':hotel_selezionato,
+            'prezzo_pagato':prezzo_finale
+        })
+        indice_hotel=hotel_ex[hotel_ex['hotel']==hotel_selezionato].index
+        hotel_ex.loc[indice_hotel, 'stanze_disponibili'] -=1
+        ospiti_allocati+=1
+        stanze_occupate+=1
+        hotel_occupati.add(hotel_selezionato)
+        guadagni_hotel[hotel_selezionato] += prezzo_finale
+        if hotel_ex['stanze_disponibili'].sum()==0:
+            st.write('Stanze sold out')
+            break
+    allocazioni_df_3 = pd.DataFrame(allocazioni)
+    guadagni_df_3=pd.DataFrame(list(guadagni_hotel.items()), columns=['Hotel', 'Guadagno Totale'])
+    numero_hotel_occupati= len(hotel_occupati)
+    st.write(f'Numero di ospiti che hanno ottenuto una camera: {ospiti_allocati}')
+    st.write(f'Numero di stanze occupate: {stanze_occupate}')
+    st.write(f'Numero di hotel occupati: {len(hotel_occupati)}')
+    st.write(f'Ospiti soddisfatti: {ospiti_soddisfatti}')
+    st.subheader("Guadagni totali per ogni hotel")
+    st.dataframe(guadagni_df_3)
+    st.subheader("Allocazioni degli ospiti")
+    st.dataframe(allocazioni_df_3)
+
+    fig, ax = plt.subplots()
+    ax.pie([ospiti_soddisfatti, len(guest_ex) - ospiti_soddisfatti], labels=['Ospiti soddisfatti', 'Ospiti non soddisfatti'], colors=['green', 'red'], autopct='%1.1f%%')
+    ax.set_title('Soddisfazione degli Ospiti')
+    st.pyplot(fig)
+
 # Selettore di modulo
-metodo_selezionato = st.selectbox("Seleziona il metodo da visualizzare", ("random", "customer_preference"))
+metodo_selezionato = st.selectbox("Seleziona il metodo da visualizzare", ("random", "customer_preference", "price"))
 
 # Visualizza il modulo selezionato
 if metodo_selezionato == "random":
     random()
 elif metodo_selezionato == "customer_preference":
     customer_preference()
+elif metodo_selezionato == "price":
+    price()
